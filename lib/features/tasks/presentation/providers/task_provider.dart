b93514ccbf08272acf90d2efa5f87d9014763e0c
Mansoor Satti <mansoorsatti201@gmail.com>
2026-09-10 23:07:30 +0500

    feat: wrap app in ProviderScope and add task_provider with loadTasks

---
+++ lib/features/tasks/presentation/providers/task_provider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/di/injection.dart';
+import '../../domain/entities/task_entity.dart';
+import '../../domain/usecases/create_task_usecase.dart';
+import '../../domain/usecases/delete_task_usecase.dart';
+import '../../domain/usecases/get_tasks_usecase.dart';
+import '../../domain/usecases/update_task_usecase.dart';
+
+final getTasksUseCaseProvider = Provider<GetTasksUseCase>((ref) {
+  return Injection.getTasksUseCase;
+});
+
+final createTaskUseCaseProvider = Provider<CreateTaskUseCase>((ref) {
+  return Injection.createTaskUseCase;
+});
+
+final updateTaskUseCaseProvider = Provider<UpdateTaskUseCase>((ref) {
+  return Injection.updateTaskUseCase;
+});
+
+final deleteTaskUseCaseProvider = Provider<DeleteTaskUseCase>((ref) {
+  return Injection.deleteTaskUseCase;
+});
+
+class TaskNotifier extends StateNotifier<AsyncValue<List<TaskEntity>>> {
+  final GetTasksUseCase getTasksUseCase;
+  final CreateTaskUseCase createTaskUseCase;
+  final UpdateTaskUseCase updateTaskUseCase;
+  final DeleteTaskUseCase deleteTaskUseCase;
+
+  TaskNotifier({
+    required this.getTasksUseCase,
+    required this.createTaskUseCase,
+    required this.updateTaskUseCase,
+    required this.deleteTaskUseCase,
+  }) : super(const AsyncValue.loading());
+
+  Future<void> loadTasks() async {
+    state = const AsyncValue.loading();
+
+    state = await AsyncValue.guard(() async {
+      return getTasksUseCase();
+    });
+  }
+}
+
+final taskProvider = StateNotifierProvider<
+    TaskNotifier,
+    AsyncValue<List<TaskEntity>>
+>((ref) {
+  return TaskNotifier(
+    getTasksUseCase: ref.read(getTasksUseCaseProvider),
+    createTaskUseCase: ref.read(createTaskUseCaseProvider),
+    updateTaskUseCase: ref.read(updateTaskUseCaseProvider),
+    deleteTaskUseCase: ref.read(deleteTaskUseCaseProvider),
+  );
+});
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/di/injection.dart';
 
@@ -10,7 +11,11 @@ Future<void> main() async {
 
   Injection.init();
 
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
